d5d26915d278533d23cd3b3b91a3489a51586345
NewUser <newuser@MacBook-Pro-2.hitronhub.home>
2024-02-27 15:54:28 -0500

    Authenticated user links Done

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,17 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-
+import UsernameManu from "./UsernameManu"
 const MainNav = () => {
-    const {loginWithRedirect} = useAuth0();
+    const {loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    // ghost removes most of the styles so can add a few of our own
-    <Button 
-        variant="ghost" 
-        className="font-bold hover:text-orange-500 hover:bg-white"
-        onClick={async () => await loginWithRedirect()}
-    >
-        Log In
-    </Button>
+    <span className=" \flex space-x-2 items-center">
+        {isAuthenticated ? <UsernameManu/> : 
+            <Button 
+            // ghost removes most of the styles so can add a few of our own
+            variant="ghost" 
+            className="font-bold hover:text-orange-500 hover:bg-white"
+            onClick={async () => await loginWithRedirect()}
+        >
+            Log In
+        </Button>
+        }
+    </span>
   )
 }
 export default MainNav;

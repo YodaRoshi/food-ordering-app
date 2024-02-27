e6895dd788c0eb0b956a9db633c9aebeb92ddceb
NewUser <newuser@MacBook-Pro-2.hitronhub.home>
2024-02-27 14:33:27 -0500

    Auth0 Setup Done

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,11 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+    const {loginWithRedirect} = useAuth0();
   return (
     // ghost removes most of the styles so can add a few of our own
     <Button 
         variant="ghost" 
         className="font-bold hover:text-orange-500 hover:bg-white"
+        onClick={async () => await loginWithRedirect()}
     >
         Log In
     </Button>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
         <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-            <h1 className="text-5xl font-bold trackin-tight text-orange-600">
+            <h1 className="text-5xl font-bold tracking-tight text-orange-600">
                 Tuck into takeway today
             </h1>
             <span className="text-xl">Food is just a click away</span>
@@ -12,7 +12,7 @@ const HomePage = () => {
         <div className="grid md:grid-cols-2 gap-5">
             <img src={landingImage} />
             <div className="flex flex-col items-center justify-center gap-4 text-center">
-                <span className="font -bold text-3xl tracking-tighter">
+                <span className="font-bold text-3xl tracking-tighter">
                     Order takeaway even faster!
                 </span>
                 <span>

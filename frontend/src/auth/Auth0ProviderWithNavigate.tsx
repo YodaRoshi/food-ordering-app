0c508068dbec99ebd56bb5bca1828f221c308b57
NewUser <newuser@MacBook-Pro-2.hitronhub.home>
2024-03-03 16:48:25 -0500

    Create User Request Done

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from '@/api/MyUserApi';
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 }
 
 const Auth0ProviderWithNavigate = ({children}: Props) => {
+    const {createUser } = useCreateMyUser();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -14,14 +16,16 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
     }
     // This what gets fired whenever the user gets sent back to us after the login.
     const onRedirectCallback = (appState?: AppState, user?:User) => {
-            console.log("USER", user)
+            if(user?.sub && user?.email){
+                createUser({auth0Id: user.sub, email: user.email})
+            }
     }
     return (
         <Auth0Provider 
         domain={domain} 
         clientId={clientId} 
         authorizationParams={{
-            redirect_uri: redirectUri
+        redirect_uri: redirectUri
         }}
         onRedirectCallback={onRedirectCallback}
         >

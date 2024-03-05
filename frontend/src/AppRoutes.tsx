6a6e4ebb9f4bdbf1455725056568327679ece2f4
NewUser <newuser@MacBook-Pro-2.hitronhub.home>
2024-03-05 18:25:34 -0500

    User Profile Form Done

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,13 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/layout"
 import HomePage from "./pages/HomePage"
+import UserProfilePage from "./pages/UserProfilePage"
 import AuthCallbackPage from "./pages/AuthCallbackPage"
 const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/" element={<Layout><HomePage/></Layout>} />
-            <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+            <Route path="/" element={<Layout showHero><HomePage/></Layout>} />
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />            
+            <Route 
+                path="/user-profile" 
+                element={
+                    <Layout>
+                        <UserProfilePage />
+                    </Layout>
+                } 
+            />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
